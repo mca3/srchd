de4f8841f5cca5864a616f39a1e8a0010817e905
mca <mca@int21h.xyz>
2024-01-11 10:16:30 -0400

    merge results based upon links

--- searcher.go
+++ searcher.go
@@ -20,6 +20,39 @@ func findWantedEngines(c *mwr.Ctx) []string {
 	return strings.Split(request, ";")
 }
 
+func normalizeLink(link string) string {
+	// TODO: Properly
+	return strings.TrimSuffix(link, "/")
+}
+
+// Drops result entries if the link was already seen earlier in the result slice.
+func mergeResults(res []search.Result) []search.Result {
+	// Track the first time we see a link and move stuff around.
+	firstSeen := map[string]int{}
+
+	for i := 0; i < len(res); i++ {
+		link := normalizeLink(res[i].Link)
+
+		_, ok := firstSeen[link]
+		if !ok {
+			// First occurrence.
+			firstSeen[link] = i
+			continue
+		}
+
+		// Swap with the last element and shrink the slice.
+		res[i], res[len(res)-1] = res[len(res)-1], res[i]
+		res = res[:len(res)-1]
+
+		// Decrement i so we can try the next element.
+		i--
+	}
+
+	// Return the (modified) slice.
+	return res
+}
+
+// Searches all requested engines.
 func doSearch(c *mwr.Ctx, category search.Category, query string, page int) ([]search.Result, error) {
 	wg := sync.WaitGroup{}
 
@@ -50,5 +83,6 @@ func doSearch(c *mwr.Ctx, category search.Category, query string, page int) ([]s
 	}
 
 	wg.Wait()
-	return results, nil
+
+	return mergeResults(results), nil
 }
---
+++ searcher_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+
+	"git.int21h.xyz/srchd/search"
+)
+
+func TestMergeResults(t *testing.T) {
+	results := []search.Result{
+		{Title: "1", Link: "1"},
+		{Title: "3", Link: "3"},
+		{Title: "2", Link: "2"},
+		{Title: "3", Link: "3"},
+		{Title: "1", Link: "1"},
+		{Title: "2", Link: "2"},
+	}
+
+	results = mergeResults(results)
+
+	for i, link := range []string{"1", "3", "2"} {
+		res := results[i].Link
+		if res != link {
+			t.Errorf(`results[%d] = %q, not %q`, i, res, link)
+		}
+	}
+}
